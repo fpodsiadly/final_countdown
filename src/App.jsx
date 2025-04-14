b5cd6f360850c063aa1bfde575437f6059af92ee
Filip <fpodsiadly@gmail.com>
2025-04-14 17:35:21 +0200

    add TimerChallange component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import Player from './components/Player.jsx';
+import Player from './components/Player.jsx'
+import TimerChallange from './components/TimerChallange.jsx'
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallange title="First Challange" targetTime={1} />
+        <TimerChallange title="Second Challange" targetTime={5} />
+        <TimerChallange title="Third Challange" targetTime={10} />
+        <TimerChallange title="Fourth Challange" targetTime={15} />
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
