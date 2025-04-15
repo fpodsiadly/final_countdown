57f8ee5949e2074f46c0490786d7bf23024a0fb8
Filip <fpodsiadly@gmail.com>
2025-04-15 12:40:53 +0200

    use useRef for timer management and improve button functionality

--- src/components/TimerChallange.jsx
+++ src/components/TimerChallange.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 export default function TimerChallange({ title, targetTime }) {
+  const timer = useRef()
   const [timerStarted, setTimerStarted] = useState(false)
   const [timerExpired, setTimerExpired] = useState(false)
 
   function handleStart() {
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true)
     }, targetTime * 1000)
     setTimerStarted(true)
   }
 
   function handleStop() {
+    clearTimeout(timer.current)
     setTimerStarted(false)
-    setTimerExpired(false)
   }
 
   return (
@@ -23,7 +24,7 @@ export default function TimerChallange({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? 'Stop' : 'Start'} Challange
         </button>
       </p>
