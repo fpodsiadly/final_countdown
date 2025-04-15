6e8afe4526f7388207e198d5e885e4bb6fd6b07c
Filip <fpodsiadly@gmail.com>
2025-04-15 18:01:50 +0200

    update ResultModal to handle remaining time and reset functionality

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,10 +1,14 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react'
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, remainingTime, onReset },
   ref
 ) {
   const dialog = useRef()
+
+  const userLost = remainingTime <= 0
+  const formettedTime = (remainingTime / 1000).toFixed(2)
+
   useImperativeHandle(ref, () => ({
     open() {
       dialog.current.showModal()
@@ -13,14 +17,15 @@ const ResultModal = forwardRef(function ResultModal(
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+      {userLost && <h2>You lost</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{' '}
+        <strong>{formettedTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
