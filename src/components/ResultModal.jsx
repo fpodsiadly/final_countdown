ae069398772a008496359c71cbcad1f9d6c9a868
Filip <fpodsiadly@gmail.com>
2025-04-22 12:27:38 +0200

    use Portal in displaying modal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react'
+import { createPortal } from 'react-dom'
 
 const ResultModal = forwardRef(function ResultModal(
   { targetTime, remainingTime, onReset },
@@ -7,30 +8,33 @@ const ResultModal = forwardRef(function ResultModal(
   const dialog = useRef()
 
   const userLost = remainingTime <= 0
-  const formettedTime = (remainingTime / 1000).toFixed(2)
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
   const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100)
 
-  useImperativeHandle(ref, () => ({
-    open() {
-      dialog.current.showModal()
-    },
-  }))
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal()
+      },
+    }
+  })
 
-  return (
-    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+  return createPortal(
+    <dialog ref={dialog} className="result-modal">
       {userLost && <h2>You lost</h2>}
-      {!userLost && <h2>You score: {score}</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
         You stopped the timer with{' '}
-        <strong>{formettedTime} seconds left.</strong>
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
   )
 })
 
